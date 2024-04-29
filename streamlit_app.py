08d0bd1547b917cf662c1629f0e83d1a6c3c237f
Kate Fang <122835750+sfc-gh-kfang@users.noreply.github.com>
2024-04-29 19:08:21 +1000

    Update streamlit_app.py
    edit

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,7 @@ my_cur = my_cnx.cursor()
 
 # run a snowflake query and put it all in a var called my_catalog
 my_cur.execute("select color_or_style from catalog_for_website")
+
 my_catalog = my_cur.fetchall()
 
 # put the dafta into a dataframe
